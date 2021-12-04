82a00e0dd101ee1dedb67363b75933d8537722eb
Anmol Madaan <56681449+anmol78961@users.noreply.github.com>
2021-12-05 01:07:30 +0400

    Create Selcection_Sort.java

---
+++ Java/Selcection_Sort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void sort (int[] arr) {
+        int arr_len = arr.length;
+
+        for (int i=0; i<arr_len; i++) {
+            int min_index = i;
+            for (int j=i+1; j<arr_len; j++) {
+                if (arr[j] < arr[min_index]) {
+                    min_index = j;
+                }
+            }
+            swap(i, min_index, arr);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void swap (int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
