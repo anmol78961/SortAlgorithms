640883e78fc339df7ce595ee8455efb438647fb9
Anmol Madaan <56681449+anmol78961@users.noreply.github.com>
2021-12-13 15:15:24 +0400

    Create Main.java

---
+++ Java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 5, 6, 1};
+	    SelectionSort.sort(arr);
+	    BubbleSort.sort(arr);
+	    RecursiveBubbleSort.sort(arr);
+    }
+}
