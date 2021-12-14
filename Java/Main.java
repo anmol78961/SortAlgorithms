d22b630bc9c71e8faf0cabd0fc0d6f5b5694b711
Anmol Madaan <56681449+anmol78961@users.noreply.github.com>
2021-12-14 15:09:49 +0400

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -2,9 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {4, 3, 2, 5, 6, 1};
-	    SelectionSort.sort(arr);
-	    BubbleSort.sort(arr);
-	    RecursiveBubbleSort.sort(arr);
-	    InsetionSort.sort(arr);
+        int arr_len = arr.length;
+	    SelectionSort.sort(arr, arr_len);
+	    BubbleSort.sort(arr, arr_len);
+	    RecursiveBubbleSort.sort(arr, arr_len);
+	    InsertionSort.sort(arr, arr_len);
     }
 }
