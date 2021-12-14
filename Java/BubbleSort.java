94945e81e2eac679457c3d08965ccc8355d343bb
Anmol Madaan <56681449+anmol78961@users.noreply.github.com>
2021-12-14 15:08:48 +0400

    Update BubbleSort.java

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -1,15 +1,15 @@
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static void sort (int[] arr) {
-        int arr_len = arr.length;
+    public static void sort (int[] arr, int arr_len) {
+        
         for (int i=0; i<arr_len; i++) {
             for (int j=0; j<arr_len - i - 1; j++) {
                 if (arr[j] > arr[j+1]) {
                     Swap.swap(j, j+1, arr);
                 }
             }
-        }
+        } 
         System.out.println(Arrays.toString(arr));
     }
 }
