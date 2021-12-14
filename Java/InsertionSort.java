954fbd8b56dfe019a4309e9effa1bccdc9d6979d
Anmol Madaan <56681449+anmol78961@users.noreply.github.com>
2021-12-14 15:07:50 +0400

    Create InsertionSort.java

---
+++ Java/InsertionSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void sort (int[] arr, int arr_len) {
+
+        for (int i=0; i<arr_len; i++) {
+            int min_index = i;
+            for (int j = i+1; j<arr_len; j++) {
+                if (arr[j] < arr[min_index]) {
+                    min_index = j;
+                }
+            }
+            Swap.swap(i, min_index, arr);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
